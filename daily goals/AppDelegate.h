91f5f8023b43605c446d67fa068e0f1bda823d36
Kosturko <205180@dig-kostj.nyhq.nytint.com>
2013-02-15 11:52:00 -0500

    -first commit

--- daily goals/AppDelegate.h
+++ daily goals/AppDelegate.h
@@ -8,8 +8,22 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    NSMutableDictionary *myDictionary;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+- (IBAction)check1:(NSButton *)sender;
+- (IBAction)check2:(NSButton *)sender;
+- (IBAction)check3:(NSButton *)sender;
+
+@property (weak) IBOutlet NSTextField *text1;
+@property (weak) IBOutlet NSTextField *text2;
+@property (weak) IBOutlet NSTextField *text3;
+
+- (IBAction)text1action:(NSTextField *)sender;
+- (IBAction)text2action:(NSTextField *)sender;
+- (IBAction)text3action:(NSTextField *)sender;
 
 @end
--- daily goals/AppDelegate.m
+++ daily goals/AppDelegate.m
@@ -12,7 +12,89 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+
+    myDictionary = [[NSMutableDictionary alloc] init];
+
     // Insert code here to initialize your application
 }
+
+- (IBAction)check1:(NSButton *)sender {
+    [self markDoneUndone:sender :_text1];
+}
+
+- (IBAction)check2:(NSButton *)sender {
+    [self markDoneUndone:sender :_text2];
+}
+- (IBAction)check3:(NSButton *)sender {
+    [self markDoneUndone:sender :_text3];
+}
+
+- (IBAction)text1action:(NSTextField *)sender {
+    [self updateDataObjectwithNewInfo:sender boxNumber:@"1"];
+}
+
+- (IBAction)text2action:(NSTextField *)sender {
+        [self updateDataObjectwithNewInfo:sender boxNumber:@"2"];
+}
+
+- (IBAction)text3action:(NSTextField *)sender {
+        [self updateDataObjectwithNewInfo:sender boxNumber:@"3"];
+}
+
+#pragma View: Mark Undone/Done Goal
+- (void)markDoneUndone:(NSButton *)checkMark :(NSTextField *)textfield {
+    if (checkMark.state == 1) {
+        textfield.textColor = [NSColor grayColor];
+    }
+    else {
+        textfield.textColor = [NSColor blackColor];
+    }
+}
+
+
+#pragma View: Update Interface with saved Data (init)
+- (void)updateUIwithSavedData {
+    
+}
+
+#pragma Controller: Update data Object with saved data (if any)
+- (void)updateDataObjectWithSavedData {
+    myDictionary = [self getDataFromFile];
+}
+
+#pragma Controller: Update Dictionary object with new goal information
+- (void)updateDataObjectwithNewInfo:(NSTextField *)sender boxNumber:(NSString *)stringNumber {
+
+    NSString *myWord = sender.stringValue;
+    NSString *theInt = stringNumber;
+    NSNumber *value = [myDictionary objectForKey:myWord];
+    
+    if (value) {
+        NSNumber *nextValue = [NSNumber numberWithInt:[value intValue] + 1];
+        [myDictionary setObject:nextValue  forKey:myWord];
+    }
+    else {
+        [myDictionary setObject:myWord forKey:theInt];
+    }
+
+    //Controller pass info to Model to store and retrieve data
+}
+
+#pragma Model: Get Data from File (If it exists)
+- (NSMutableDictionary *)getDataFromFile {
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"file.txt"];
+    NSMutableDictionary *savedStock = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
+    
+    return savedStock;
+}
+
+#pragma Model:Save Data to text File
+- (void)saveDatatoFile {
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"file.txt"];
+    [myDictionary writeToFile:filePath atomically:YES];
+}
+
 
 @end
